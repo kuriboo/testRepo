66751fea337b46ea2391a20f24af93214781c7f9
kuriboo <kuriboo5@gmail.com>
2024-11-01 14:20:51 +0900

    Add src/test2.tsx

--- src/test2.tsx
+++ src/test2.tsx
@@ -1,147 +1,112 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-type SystemSettingsProps = {
-  businessLocations: string[];
-  userLocations: string[];
-  commonMessages: string;
-  hotelFile: string;
-  ryokanFile: string;
-  tempDirectory: string;
-  keiseiMyoshinDirectory: string;
-  keiseiRyokanDirectory: string;
-  dataDirectory: string;
+type SystemSettingProps = {
+  businessCategory?: string;
+  receiptFileLocation?: string;
+  quotationFileLocation?: string;
+  purchaseFileLocation?: string;
+  orderSlipFileLocation?: string;
+  yearEndClosingBatchFileLocation?: string;
+  accountingDataFileLocation?: string;
 };
 
-const SystemSettings: React.FC<SystemSettingsProps> = ({
-  businessLocations,
-  userLocations,
-  commonMessages,
-  hotelFile,
-  ryokanFile,
-  tempDirectory,
-  keiseiMyoshinDirectory,
-  keiseiRyokanDirectory,
-  dataDirectory,
+const SystemSetting: React.FC<SystemSettingProps> = ({
+  businessCategory = '共通設定',
+  receiptFileLocation = 'C:\\Gyosei\\Ryokin',
+  quotationFileLocation = 'C:\\Gyosei\\Mitsumori',
+  purchaseFileLocation = 'C:\\Gyosei\\Siire',
+  orderSlipFileLocation = 'C:\\temp',
+  yearEndClosingBatchFileLocation = 'C:\\Gyosei\\Ryokin\\System',
+  accountingDataFileLocation = 'C:\\Gyosei\\Ryokin',
 }) => {
   return (
     <Container>
-      <Title>その他</Title>
+      <Title>システム設定</Title>
+      
       <Section>
-        <SectionTitle>業務メッセージ</SectionTitle>
-        <TextField value={commonMessages} readOnly />
-      </Section>
-      <Section>
-        <SectionTitle>シミュレーションパス</SectionTitle>
-        <PathList>
-          {businessLocations.map((location, index) => (
-            <PathItem key={index}>
-              <span>{index + 1}</span>
-              <TextField value={location} readOnly />
-            </PathItem>
-          ))}
-        </PathList>
-      </Section>
-      <Section>
-        <SectionTitle>料金改定</SectionTitle>
-        <PathItem>
-          <span>旅館</span>
-          <TextField value={hotelFile} readOnly />
-        </PathItem>
-        <PathItem>
-          <span>シティホテル</span>
-          <TextField value={ryokanFile} readOnly />
-        </PathItem>
-      </Section>
-      <Section>
-        <SectionTitle>年次更新 不納欠損パス</SectionTitle>
-        <TextField value={tempDirectory} readOnly />
-      </Section>
-      <Section>
-        <SectionTitle>受水槽対象者一覧表パス</SectionTitle>
-        <PathItem>
-          <TextField value={keiseiMyoshinDirectory} readOnly />
-        </PathItem>
-      </Section>
-      <Section>
-        <SectionTitle>会計連携データパス</SectionTitle>
-        <TextField value={keiseiRyokanDirectory} readOnly />
-      </Section>
-      <Section>
-        <SectionTitle>地区種別</SectionTitle>
-        <PathList>
-          {userLocations.map((location, index) => (
-            <PathItem key={index}>
-              <span>{index + 1}</span>
-              <TextField value={location} readOnly />
-            </PathItem>
-          ))}
-        </PathList>
+        <SectionTitle>その他</SectionTitle>
+        <Table>
+          <tbody>
+            <TableRow>
+              <TableHeader>業務カテゴリ</TableHeader>
+              <TableData>{businessCategory}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>領収書</TableHeader>
+              <TableData>{receiptFileLocation}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>見積書</TableHeader>
+              <TableData>{quotationFileLocation}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>仕入</TableHeader>
+              <TableData>{purchaseFileLocation}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>年次更新・不納欠損バス</TableHeader>
+              <TableData>{orderSlipFileLocation}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>受水槽対象者一覧表バス</TableHeader>
+              <TableData>{yearEndClosingBatchFileLocation}</TableData>
+            </TableRow>
+            <TableRow>
+              <TableHeader>会計連携データバス</TableHeader>
+              <TableData>{accountingDataFileLocation}</TableData>
+            </TableRow>
+          </tbody>
+        </Table>
       </Section>
     </Container>
   );
 };
 
 const Container = styled.div`
-  background-color: #f0f0f0;
-  padding: 16px;
-  width: 600px;
+  padding: 24px;
 `;
 
 const Title = styled.h2`
-  font-size: 18px;
+  font-size: 24px;
   margin-bottom: 16px;
 `;
 
 const Section = styled.div`
-  margin-bottom: 24px;
+  margin-bottom: 32px;
 `;
 
 const SectionTitle = styled.h3`
-  font-size: 16px;
+  font-size: 20px;
   margin-bottom: 8px;
 `;
 
-const PathList = styled.ul`
-  list-style: none;
-  padding: 0;
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
 `;
 
-const PathItem = styled.li`
-  display: flex;
-  align-items: center;
-  margin-bottom: 8px;
-
-  > span {
-    margin-right: 8px;
+const TableRow = styled.tr`
+  &:nth-of-type(even) {
+    background-color: #f2f2f2;
   }
 `;
 
-const TextField = styled.input`
-  width: 100%;
-  padding: 4px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 14px;
+const TableHeader = styled.th`
+  padding: 8px;
+  text-align: left;
+  font-weight: bold;
 `;
 
-const SampleData: SystemSettingsProps = {
-  businessLocations: ['C:￥Gyosei￥Ryokan', 'D:￥Gyosei￥Ryokan'],
-  userLocations: ['横浜', '箱根', '調布', '千葉', '木更津', '富士', '戸田', '両会津'],
-  commonMessages: 'C:￥Gyosei￥Ryokin',
-  hotelFile: 'C:￥hotelFile.xls',
-  ryokanFile: 'C:￥RyokanFile.xls',
-  tempDirectory: 'C:￥temp',
-  keiseiMyoshinDirectory: 'C:￥Gyosei￥Ryokin￥System',
-  keiseiRyokanDirectory: 'C:￥Gyosei￥Ryokan',
-  dataDirectory: 'コース地区',
-};
+const TableData = styled.td`
+  padding: 8px;
+`;
 
+// Usage example
 const App: React.FC = () => {
   return (
     <div>
-      <h1>System Settings Example</h1>
-      <SystemSettings {...SampleData} />
+      <SystemSetting />
     </div>
   );
 };
